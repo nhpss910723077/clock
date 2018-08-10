5741f971d7425501c81dbbae7b7f9bb5b3ab3d80
nhpss910723077 <nhpss91064@gmail.com>
2018-08-10 23:51:13 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    function time() {
+        var now = new Date();
+        var hour = timeFormat(now.getHours());
+        var min = timeFormat(now.getMinutes());
+        var sec = timeFormat(now.getSeconds());
+        var ampm = "";
+        if (hour > 12) {
+            ampm = "下午"
+            hour = hour - 12;            
+        } else {
+            ampm = "上午"
+        };
+
+        $(".time").html(ampm+" "+hour + ":" + min + ":" + sec);
+
+        requestAnimationFrame(time);
+    }
+
+    function timeFormat(i) {
+        if (i < 10) {
+            i = "0" + i;
+        };
+        return i;
+    };
+
+    time();
+});
